df260fb2b379aaf2202df33c0f1442f136a8a9b6
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2021-02-28 00:11:41 +0600

    Product Create & Update Endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -96,4 +96,10 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,8 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -12,7 +14,11 @@ router.route("/").get(getProducts);
 // OR ALTERNATE WAY
 // router.get('/', getProducts)
 
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 // OR ALTERNATE WAY
 // router.get('/:id', getProductById)
 
