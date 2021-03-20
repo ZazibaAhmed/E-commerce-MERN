2c70e730059eaa4f84772d5a445949e33e772dc8
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2021-03-20 22:49:23 +0600

    Front-End Img Upload

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,60 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import { notFound, errorHandler} from './middleware/errorMiddleware.js' 
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
+dotenv.config();
 
-dotenv.config() 
+connectDB();
 
-connectDB()
-
-const app = express()
+const app = express();
 
 //Will allow us to accept JSON data in the body
-app.use(express.json())
+app.use(express.json());
 
 // app.use( (req, res, next) => {
 //     console.log("Original URL: "+req.originalUrl);
 //     next()
 // })
 
-app.get('/', (req, res) => {
-    res.send("API is running...");
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 // Anything that goes to the link wil be linked to this route
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 //Basically like a config route
-app.get('/api/config/paypal', (req, res) => 
-    res.send(process.env.PAYPAL_CLIENT_ID) 
-)
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+//making the uploads folder in frontend static so it can be uploaded in the browser
+//so we need path module for that
+//__dirname in nodeJS points to the current directory but not availbe in ES^ modules, availbael only in common JS
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Handling error
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT= process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Server Listens to Port
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
