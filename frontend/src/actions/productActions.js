84eda3864dd23313fb43ec3c372947dda9563735
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2021-03-22 23:56:07 +0600

    Product reviews done

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,8 +14,15 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_SUCCESS,
+  PRODUCT_TOP_FAIL,
 } from "../constants/productConstants";
 import axios from "axios";
+import { logout } from "./userActions";
 
 //These functions can be considered action creators
 //Disptach the actions to reducers
@@ -190,6 +197,46 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     }
     dispatch({
       type: PRODUCT_UPDATE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const createProductReview = (productId, review) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.post(`/api/products/${productId}/reviews`, review, config);
+
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAIL,
       payload: message,
     });
   }
